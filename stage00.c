69a677a424c100af94f336ef827d02ceb117c761
Daniel Savage <dansvg@gmail.com>
2020-10-18 20:04:22 -0700

    I was a big kid and fixed my cube DL

--- stage00.c
+++ stage00.c
@@ -67,9 +67,11 @@ static Vtx cube_geo[] = {
 static Gfx blue_cube_commands[] = {
   gsSPVertex(&cube_geo, 8, 0),
   gsSP2Triangles(0, 1, 2, 0, 0, 2, 3, 0),
-  gsSP2Triangles(3, 2, 6, 0, 3, 6, 7, 0),
-  gsSP2Triangles(1, 0, 4, 0, 1, 4, 5, 0),
-  gsSP2Triangles(4, 5, 6, 0, 5, 6, 7, 0),
+  gsSP2Triangles(4, 5, 0, 0, 5, 1, 0, 0),
+  gsSP2Triangles(5, 6, 2, 0, 5, 2, 1, 0),
+  gsSP2Triangles(7, 4, 0, 0, 7, 0, 3, 0),
+  gsSP2Triangles(6, 4, 7, 0, 6, 5, 4, 0),
+  gsSP2Triangles(6, 7, 3, 0, 6, 3, 2, 0),
   gsSPEndDisplayList()
 };
 
